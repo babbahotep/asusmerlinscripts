569180183720e4798616a8da7620158b47f701a0
babbahotep <3181634+babbahotep@users.noreply.github.com>
2019-04-29 15:29:39 -0700

    rewrote esxi backup script to chekc if host is up

--- exec_on_asus/esxibackup/backupesxihost.sh
+++ exec_on_asus/esxibackup/backupesxihost.sh
@@ -1,106 +1,99 @@
 #!/bin/bash
 # ./backuphost.sh user hostip
-#test 
+# id-rsa.pub on every host
+# esxi needs ssh -i /path/to/id_rsa
 user="root"
 host1="192.168.1.120"
 host2="192.168.1.130"
 host3="192.168.1.140"
+stamp=`date '+%Y_%m_%d-%H-%M'`
 sshx="/usr/bin/ssh -i /home/root/.ssh/id_rsa "
 echo "user:" "$user" "and" "host ip:" "$host1" "$host2" "$host3"
+echo "date" $stamp
 
+# Host 1
+echo "check if next $host1 is up"
+if ping -c1 $host1 1>/dev/null 2>/dev/null
+then
+  echo "$host1 is up"
+  ESXI_VERSION_BASE=$($sshx $user@$host1 vmware -v | awk '{ print $3 }' | sed "s/\./-/g")
+  echo "base" $ESXI_VERSION_BASE
+  sleep 3
+  ESXI_VERSION_BUILD=$($sshx $user@$host1 vmware -v | awk '{ print $4 }')
+  echo "build" $ESXI_VERSION_BUILD
+  sleep 3
+  # sync Config
+  echo "Sync Config on ESXI host"
+  $sshx $user@$host1 vim-cmd hostsvc/firmware/sync_config
+  echo "Backup and download ESXI host config"
+  URL1=$($sshx $user@$host1 vim-cmd hostsvc/firmware/backup_config)
+  path1="$(echo $URL1 | cut -d "*" -f 2)"
+  proto1=$(echo $URL1 | grep -Eo "(http|https)://[a-zA-Z0-9./?=_-]*")
+  xfile1=$(echo "${URL1##*/}")
+  #xfile21=$(echo $stamp"_"$host1"__"$xfile1)
+  xfile21=${stamp}_"base"_${ESXI_VERSION_BASE}-${ESXI_VERSION_BUILD}_${host1}__${xfile1}
+  echo "filename" $xfile21
+  sleep 3
+  hosturl1=$(echo $proto1$host1$path1)
+  wget --no-check-certificate --continue $hosturl1 -O $xfile1
+  mv -f $xfile1 /mnt/ASUS32GB/esxibackups/120/$xfile21
+else
+  echo "$host1 is down and nothing here"
+fi
+
+# Host 2
+echo "check if next $host2 is up"
+if ping -c1 $host1 1>/dev/null 2>/dev/null
+then
+  echo "$host2 is up"
+  ESXI_VERSION_BASE=$($sshx $user@$host1 vmware -v | awk '{ print $3 }' | sed "s/\./-/g")
+  echo "base" $ESXI_VERSION_BASE
+  ESXI_VERSION_BUILD=$($sshx $user@$host1 vmware -v | awk '{ print $4 }')
+  echo "build" $ESXI_VERSION_BUILD
+  # sync Config
+  echo "Sync Config on ESXI host"
+  $sshx $user@$host2 vim-cmd hostsvc/firmware/sync_config
+  echo "Backup and download ESXI host config"
+  URL2=$($sshx $user@$host2 vim-cmd hostsvc/firmware/backup_config)
+  path2="$(echo $URL2 | cut -d "*" -f 2)"
+  proto2=$(echo $URL2 | grep -Eo "(http|https)://[a-zA-Z0-9./?=_-]*")
+  xfile2=$(echo "${URL2##*/}")
+  xfile22=${stamp}_"base"_${ESXI_VERSION_BASE}-${ESXI_VERSION_BUILD}_${host2}__${xfile2}
+  echo "filename" $xfile22
+  hosturl2=$(echo $proto2$host2$path2)
+  wget --no-check-certificate --continue $hosturl2 -O $xfile2
+  mv -f $xfile2 /mnt/ASUS32GB/esxibackups/130/$xfile22
+else
+  echo "$host2 is down and nothing here"
+fi
+
+# Host 3
+echo "check if next $host3 is up"
+if ping -c1 $host1 1>/dev/null 2>/dev/null
+then
+echo "$host3 is up"
+ESXI_VERSION_BASE=$($sshx $user@$host1 vmware -v | awk '{ print $3 }' | sed "s/\./-/g")
+echo "base" $ESXI_VERSION_BASE
+ESXI_VERSION_BUILD=$($sshx $user@$host1 vmware -v | awk '{ print $4 }')
+echo "build" $ESXI_VERSION_BUILD
 # sync Config
 echo "Sync Config on ESXI host"
-$sshx $user@$host1 vim-cmd hostsvc/firmware/sync_config
-echo $?
-sleep 3
-$sshx $user@$host2 vim-cmd hostsvc/firmware/sync_config
-echo $?
-sleep 3
 $sshx $user@$host3 vim-cmd hostsvc/firmware/sync_config
-echo $?
-sleep 3
-# create backup archive and download
-#ipaddr=$(hostname -i)
-stamp=`date '+%Y_%m_%d-%H-%M'`
-echo "date" $stamp
-sleep 3
 echo "Backup and download ESXI host config"
-URL1=$($sshx $user@$host1 vim-cmd hostsvc/firmware/backup_config)
-URL2=$($sshx $user@$host2 vim-cmd hostsvc/firmware/backup_config)
 URL3=$($sshx $user@$host3 vim-cmd hostsvc/firmware/backup_config)
-
-path1="$(echo $URL1 | cut -d "*" -f 2)"
-path2="$(echo $URL2 | cut -d "*" -f 2)"
 path3="$(echo $URL3 | cut -d "*" -f 2)"
-proto1=$(echo $URL1 | grep -Eo "(http|https)://[a-zA-Z0-9./?=_-]*")
-proto2=$(echo $URL2 | grep -Eo "(http|https)://[a-zA-Z0-9./?=_-]*")
 proto3=$(echo $URL3 | grep -Eo "(http|https)://[a-zA-Z0-9./?=_-]*")
-
-xfile1=$(echo "${URL1##*/}")
-xfile2=$(echo "${URL2##*/}")
 xfile3=$(echo "${URL3##*/}")
-
-xfile21=$(echo $stamp"_"$host1"__"$xfile1)
-xfile22=$(echo $stamp"_"$host2"__"$xfile2)
-xfile23=$(echo $stamp"_"$host3"__"$xfile3)
-
-hosturl1=$(echo $proto1$host1$path1)
-hosturl2=$(echo $proto2$host2$path2)
+xfile23=${stamp}_"base"_${ESXI_VERSION_BASE}-${ESXI_VERSION_BUILD}_${host3}__${xfile3}
+echo "filename" $xfile23
 hosturl3=$(echo $proto3$host3$path3)
-
-echo "date" $stamp
-echo "datefile" $xfile2
-sleep 3
-
-#echo $host
-#echo "1" $hosturl
-#echo "2" $proto$host$path
-#echo "3" $xfile
-wget --no-check-certificate --continue $hosturl1 -O $xfile1
-wget --no-check-certificate --continue $hosturl2 -O $xfile2
 wget --no-check-certificate --continue $hosturl3 -O $xfile3
-
-echo "new file name" $xfile2
-sleep 3
-mv -f $xfile1 /mnt/ASUS32GB/esxibackups/120/$xfile21
-mv -f $xfile2 /mnt/ASUS32GB/esxibackups/130/$xfile22
 mv -f $xfile3 /mnt/ASUS32GB/esxibackups/140/$xfile23
-#scp $xfile admin@192.168.1.1:/jffs/esxiscripts
+
+else
+  echo "$host3 is down and nothing here"
+fi
 
 sh /jffs/customscripts/exec_on_asus/mail.sh esxi@home.lan esxi@home.lan "esxi backup complete"  "$xfile21 $xfile22 $xfile23"
-
-#extract URL
-# Extract the protocol (includes trailing "://").
-#PARSED_PROTO="$(echo $PROJECT_URL | sed -nr 's,^(.*://).*,\1,p')"
-
-# Remove the protocol from the URL.
-#PARSED_URL="$(echo ${PROJECT_URL/$PARSED_PROTO/})"
-
-# Extract the user (includes trailing "@").
-#PARSED_USER="$(echo $PARSED_URL | sed -nr 's,^(.*@).*,\1,p')"
-
-# Remove the user from the URL.
-#PARSED_URL="$(echo ${PARSED_URL/$PARSED_USER/})"
-
-# Extract the port (includes leading ":").
-#PARSED_PORT="$(echo $PARSED_URL | sed -nr 's,.*(:[0-9]+).*,\1,p')"
-
-# Remove the port from the URL.
-#PARSED_URL="$(echo ${PARSED_URL/$PARSED_PORT/})"
-
-# Extract the path (includes leading "/" or ":").
-#PARSED_PATH="$(echo $PARSED_URL | sed -nr 's,[^/:]*([/:].*),\1,p')"
-
-# Remove the path from the URL.
-#PARSED_HOST="$(echo ${PARSED_URL/$PARSED_PATH/})"
-
-ls -al | grep config*
-
-#echo "url" $PARSED_URL
-#echo $PARSED_URL
-#echo "path" $PARSEDPATH
-#echo
-#echo
-#echo
 
 exit
