6a9095b38f7fc25369bc406422a78f4f0a4a8d64
babbahotep <3181634+babbahotep@users.noreply.github.com>
2019-04-29 17:37:40 -0700

    delete old backups

--- exec_on_asus/esxibackup/backupesxihost.sh
+++ exec_on_asus/esxibackup/backupesxihost.sh
@@ -6,8 +6,11 @@ user="root"
 host1="192.168.1.120"
 host2="192.168.1.130"
 host3="192.168.1.140"
+backuppath="/mnt/ASUS32GB/esxibackups/"
+#backuppath2="/mnt/ASUS32GB/esxibackups/"
+#backuppath2="/mnt/ASUS32GB/esxibackups/"
 stamp=`date '+%Y_%m_%d-%H-%M'`
-BACKUP_KEEP=3 # set number of days to keep
+BACKUP_KEEP=5 # set number of days to keep
 sshx="/usr/bin/ssh -i /home/root/.ssh/id_rsa "
 echo "user:" "$user" "and" "host ip:" "$host1" "$host2" "$host3"
 echo "date" $stamp
@@ -18,11 +21,11 @@ if ping -c1 $host1 1>/dev/null 2>/dev/null
 then
   echo "$host1 is up"
   ESXI_VERSION_BASE=$($sshx $user@$host1 vmware -v | awk '{ print $3 }' | sed "s/\./-/g")
-  echo "base" $ESXI_VERSION_BASE
-  sleep 3
+  #echo "base" $ESXI_VERSION_BASE
+
   ESXI_VERSION_BUILD=$($sshx $user@$host1 vmware -v | awk '{ print $4 }')
-  echo "build" $ESXI_VERSION_BUILD
-  sleep 3
+  #echo "build" $ESXI_VERSION_BUILD
+
   # sync Config
   echo "Sync Config on ESXI host"
   $sshx $user@$host1 vim-cmd hostsvc/firmware/sync_config
@@ -34,10 +37,13 @@ then
   #xfile21=$(echo $stamp"_"$host1"__"$xfile1)
   xfile21=${stamp}_"base"_${ESXI_VERSION_BASE}-${ESXI_VERSION_BUILD}_${host1}__${xfile1}
   echo "filename" $xfile21
-  sleep 3
   hosturl1=$(echo $proto1$host1$path1)
+  # download backup
   wget --no-check-certificate --continue $hosturl1 -O $xfile1
-  mv -f $xfile1 /mnt/ASUS32GB/esxibackups/120/$xfile21
+  mv -f $xfile1 $backuppath/120/$xfile21
+  # remove old backups
+  find $backuppath/120/ -mtime +$BACKUP_KEEP -type f -exec rm {} \;
+  echo 'remaining:' ls $backuppath/120/
 else
   echo "$host1 is down and nothing here"
 fi
@@ -62,8 +68,13 @@ then
   xfile22=${stamp}_"base"_${ESXI_VERSION_BASE}-${ESXI_VERSION_BUILD}_${host2}__${xfile2}
   echo "filename" $xfile22
   hosturl2=$(echo $proto2$host2$path2)
+  # Download backup
   wget --no-check-certificate --continue $hosturl2 -O $xfile2
-  mv -f $xfile2 /mnt/ASUS32GB/esxibackups/130/$xfile22
+  #rename file
+  mv -f $xfile2 $backuppath/130/$xfile22
+  #remove old backups
+  find $backuppath/130/ -mtime +$BACKUP_KEEP -type f -exec rm {} \;
+  echo 'remaining:' ls $backuppath/130/
 else
   echo "$host2 is down and nothing here"
 fi
@@ -88,23 +99,18 @@ xfile3=$(echo "${URL3##*/}")
 xfile23=${stamp}_"base"_${ESXI_VERSION_BASE}-${ESXI_VERSION_BUILD}_${host3}__${xfile3}
 echo "filename" $xfile23
 hosturl3=$(echo $proto3$host3$path3)
+#download backup
 wget --no-check-certificate --continue $hosturl3 -O $xfile3
-mv -f $xfile3 /mnt/ASUS32GB/esxibackups/140/$xfile23
+# rename backup file
+mv -f $xfile3 $backuppath/140/$xfile23
+# remove old backups
+find $backuppaths/140/ -mtime +$BACKUP_KEEP -type f -exec rm {} \;
+echo 'remaining:' ls $backuppath/140/
 
 else
   echo "$host3 is down and nothing here"
 fi
 
-# remove old backups
-find /mnt/ASUS32GB/esxibackups/120/ -mtime +$BACKUP_KEEP -type f -exec rm {} \;
-echo 'remaining:' ls /mnt/ASUS32GB/esxibackups/120/
-sleep 3
-find /mnt/ASUS32GB/esxibackups/130/ -mtime +$BACKUP_KEEP -type f -exec rm {} \;
-echo 'remaining:' ls /mnt/ASUS32GB/esxibackups/130/
-sleep 3
-find /mnt/ASUS32GB/esxibackups/140/ -mtime +$BACKUP_KEEP -type f -exec rm {} \;
-echo 'remaining:' ls /mnt/ASUS32GB/esxibackups/140/
-sleep 3
 sh /jffs/customscripts/exec_on_asus/mail.sh esxi@home.lan esxi@home.lan "esxi backup complete"  "$xfile21 /n /n $xfile22 /n /n $xfile23"
 
 exit
